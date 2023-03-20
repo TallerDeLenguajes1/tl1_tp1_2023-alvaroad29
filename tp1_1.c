0ac086342fbfecf8460721544abfc473caa829d6
Alvaro Diaz <alvaroad29012001@gmail.com>
2023-03-19 23:09:07 -0300

    Segundo commit, punteros

--- tp1_1.c
+++ tp1_1.c
@@ -2,6 +2,16 @@
 
 int main()
 {
-    printf("Hola mundo");
+    printf("Hola mundo\n");
+
+    int *pvariable,variable=10;
+    pvariable = &variable;
+
+    printf("Contenido del puntero: %d\n",*pvariable);
+    printf("Direccion de memoria almacenada por el puntero: %d\n",pvariable);
+    printf("Direccion de memoria de la variable: %d\n",&variable);
+    printf("Direccion de memoria del puntero: %d\n",&pvariable);
+    printf("Tamanio de memoria utilizado por la variable: %d\n",sizeof(int));
+
     return 0;
 }
